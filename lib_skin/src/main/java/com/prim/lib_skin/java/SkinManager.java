0a8b61dcb109b37bca58d18956e0106caf48c690
prim <prim>
2019-09-11 13:55:52 +0800

    Initial commit

---
+++ lib_skin/src/main/java/com/prim/lib_skin/java/SkinManager.java
@@ -0,0 +1,12 @@
+package com.prim.lib_skin.java;
+
+/**
+ * @author prim
+ * @version 1.0.0
+ * @desc
+ * @time 2019-09-11 - 10:30
+ * @contact https://jakeprim.cn
+ * @name PrimSkinCore
+ */
+public class SkinManager {
+}
---
+++ lib_skin/src/main/java/com/prim/lib_skin/kotlin/SkinManagerKx.kt
@@ -0,0 +1,13 @@
+package com.prim.lib_skin.kotlin
+
+/**
+ * @desc 换肤管理器
+ * @author prim
+ * @time 2019-09-10 - 22:55
+ * @contact https://jakeprim.cn
+ * @name PrimSkinCore
+ * @version 1.0.0
+ */
+object SkinManagerKx {
+
+}
